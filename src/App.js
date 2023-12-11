e4d1f87aa1b5b32a6d4d6d76fdcc72e133805440
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-12-12 00:53:08 +0400

    initial version

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 
-import React ,{ useState } from 'react';
+import React ,{ useEffect, useState } from 'react';
 
 import './App.css';
-import { Items } from './Items';
+import Axios from 'axios';
 
 function App() {
-  const [inputNumber, setInputNumber] = useState(); // State to manage input value
+  const [myData, setMyData] = useState([]); // State to manage input value
 
-  const handleInputChange = (e) => {
-    setInputNumber(parseInt(e.target.value)); // Update inputNumber state with the input value
-    setInputNumber(isNaN(e.target.value) ? 0 : e.target.value);
-  };
+  useEffect(()=>{
+    Promise.all([
+      Axios.get("https://reqres.in/api/users?page=1"),
+      Axios.get("https://reqres.in/api/users?page=2")
+    ])
+      .then((responses) => {
+        // Combine data from both responses into a single array
+        const combinedData = responses.reduce(
+          (acc, response) => [...acc, ...response.data.data],
+          []
+        );
+        setMyData(combinedData);
+      })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+}, []);
+
   return (
 <div className="App">
-  <input value={inputNumber || ''} onChange={handleInputChange} ></input>
-  <Items num={inputNumber} />
+      <div className="box">
+        {myData.map((user) => (
+          <div className="user" key={user.id}>
+            <img src={user.avatar} alt={`Avatar of ${user.first_name}`} />
+            <p>{`${user.first_name} ${user.last_name}`}</p>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
 </div>
   );
 }
--- src/Items.js
+++
@@ -1,8 +0,0 @@
-
-export const Items=(props)=>{
-let items=[]
-    for (let i = 0; i <= props.num; i++) {
-    items.push(<div key={i}>Item {i}</div>)
-    }
-    return items
-}
